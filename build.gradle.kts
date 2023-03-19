4b0955f9065f42213bab2b5b0f2a73b8b0d25436
nathanmeade <nathan.r.meade@gmail.com>
2023-03-19 15:00:48 -0400

    Project level groovy build.gradle converted to Kotlin DSL build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+ext {
+    val kotlin_version = "1.3.2"
+    val buildToolsVersion = "30.0.0"
+    val androidxAnnotationVersion = "1.2.0"
+    val robolectricVersion = "4.6.1"
+    val guavaVersion = "30.1.1-android"
+    val extTruthVersion = "1.3.0-rc01"
+    val coreVersion = "1.4.1-alpha03"
+    val extJUnitVersion = "1.1.4-alpha03"
+    val runnerVersion = "1.4.1-alpha03"
+    val espressoVersion = "3.5.0-alpha03"
+}
